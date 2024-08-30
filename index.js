7358351c1b9353d7d74e17142741576200791e6e
justripp <105530675+justripp@users.noreply.github.com>
2024-08-30 11:57:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 1920
-canvas.height = 1080
+canvas.width = 576
+canvas.height = 1024
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
